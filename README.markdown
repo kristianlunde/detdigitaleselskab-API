e4b69751ab1586c1b8b0e0165d759aa140a1b6cf
Kristian Lunde <kr.lunde@gmail.com>
2012-08-19 09:52:57 +0200

    Added the database structure and a read me file

---
+++ README.markdown
@@ -0,0 +1,21 @@
+Det Digitale Selskab API
+========================
+
+This is an API for the monthly "Det digitale selskab" event held in Sarpsborg and Fredrikstad. 
+
+Url to the site: http://det.digitaleselskab.org
+
+The API has not been deployed to the live environment yet.
+
+Installation
+------------
+
+1. Install Nodejs, (0.8.6), it is recommended to install with https://github.com/creationix/nvm or by building it manually https://github.com/joyent/node/wiki/Installation
+2. Install mysql https://github.com/felixge/node-mysql
+	- npm install mysql@2.0.0-alpha3
+3. Checkout this code 
+	- git clone https://github.com/kristianlunde/detdigitaleselskab-API.git
+4. Set up the database and configure the database connection details at the top of the app.js file.
+5. navigate to the checkout directory
+6. run: 
+	- node app.js
---
+++ database.sql
@@ -0,0 +1,76 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.24)
+# Database: detdigitaleselskab
+# Generation Time: 2012-08-19 07:50:47 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table api_log
+# ------------------------------------------------------------
+
+CREATE TABLE `api_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(1024) NOT NULL DEFAULT '',
+  `ip` varchar(39) NOT NULL DEFAULT '',
+  `method` char(6) NOT NULL DEFAULT '',
+  `headers` varchar(1024) NOT NULL DEFAULT '',
+  `requested_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table attending
+# ------------------------------------------------------------
+
+CREATE TABLE `attending` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `desc` varchar(150) DEFAULT NULL,
+  `twitter` varchar(150) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table event
+# ------------------------------------------------------------
+
+CREATE TABLE `event` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `venue` varchar(150) NOT NULL,
+  `venue_street` varchar(255) NOT NULL,
+  `map` text NOT NULL,
+  `when` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
+  `current` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
